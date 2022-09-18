16d25ab24288343370f50ccdd7a204d88cc98309
yamatatsu <the.kinnikuman@gmail.com>
2022-09-19 00:51:32 +0900

    refactor

--- packages/jws/src/lib/sign.test.ts
+++ packages/jws/src/lib/sign.test.ts
@@ -2,12 +2,19 @@ import { test, expect } from "vitest";
 import { sign as _sign } from "jws";
 import { sign } from "./sign";
 
-test("success", () => {
-  const arg = {
+test.each([
+  {
     header: { alg: "HS256" },
     payload: {},
     secretOrKey: "key",
     secret: "key",
-  } as const;
+  } as const,
+  {
+    header: { alg: "HS256" },
+    payload: "",
+    secretOrKey: "key",
+    secret: "key",
+  } as const,
+])("success", (arg) => {
   expect(sign(arg)).toBe(_sign(arg));
 });
